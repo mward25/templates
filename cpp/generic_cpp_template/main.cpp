6025bd2a36805e68a781ba3da7b1343a0c602d04
miles <milesward2004@gmail.com>
2021-11-09 14:35:41 -0800

    Added cpp templates

---
+++ cpp/generic_cpp_template/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "functions.hpp"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+
+
+int main()
+{
+	
+	
+	return 0;
+}
+
